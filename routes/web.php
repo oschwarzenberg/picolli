4e2b2d6ca2f80a99329b53ffeb6708bf8181ff85
Oliver Schwarzenberg <os@schwarzenberg.com>
2021-06-18 20:12:58 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\CreateProductController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
